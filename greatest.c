835de1afdf2fddb8869a2bc1c60ad9355d68a1ad
11gowdham <11gowdham1996@gmail.com>
2016-07-08 00:12:53 +0530

    Create greatest.c

---
+++ greatest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=6,b=5,c=7;
+if(a>b && a>c)
+printf("a is greater");
+else if(b>a && b>c)
+printf("b is greater");
+else
+printf("c is greater");
+return 0;
+}
