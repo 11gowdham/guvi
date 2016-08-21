919caf9dcf99a31727a5585f0dc70cd0c3be1769
11gowdham <11gowdham1996@gmail.com>
2016-08-21 17:14:05 +0530

    Update prime_number.java

--- prime_number.java
+++ prime_number.java
@@ -2,29 +2,36 @@ import java.util.*;
 public class prime_number {
 
 	public static void main(String[] args) {
-		long a,i,b=0;	
+		long a,i;
+		int b=0;
 		Scanner scn=new Scanner(System.in);
 		a=scn.nextLong();
 		if(a<=1)
 		{
 			System.out.println("It is not a prime number");
 		}
+		else if(a==2||a==3)
+		{
+			System.out.println("It is a prime number");
+		}
 		else if(a%2==0)
 		{
 			System.out.println("It is not a prime number");
 		}
-		else
+		else{
 		for(i=3;i<a;i=i+2)
 		{
+			b=1;
 			if(a%i==0){
 			b++;
 			break;
 			}
 		}
-		if(b>0)
-			System.out.println("It is not a prime number");
-		else
+		if(b==1)
 			System.out.println("It is a prime number");
+		else if(b==2)
+			System.out.println("It is not a prime number");
+		}
 	}
 
 }
