6506714c77550a87ac67298b25e2cc60efccc4d7
11gowdham <11gowdham1996@gmail.com>
2016-08-22 09:36:18 +0530

    Create odd_R_even_without_%.java

---
+++ odd_R_even_without_%.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class odd_R_even_without_sym {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int a;
+		a=scn.nextInt();
+		if((a/2)*2==a){
+			System.out.println("It is even number");
+			}
+		else{
+			System.out.println("It is odd number");
+		}
+	}
+
+}
